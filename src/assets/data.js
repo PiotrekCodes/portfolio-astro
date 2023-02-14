80d0b4b5cc375cf94c29add03208f1ddc9c5e2cd
Piotr Krawczyk <sztymperjan@gmail.com>
2023-02-14 21:12:42 +0000

    about and project card added

---
+++ src/assets/data.js
@@ -0,0 +1,18 @@
+const data = [
+  {
+    name: 'Ouicity',
+    url: 'http://www.ouicity.me/',
+    github: 'https://github.com/DariBerrie/ouicity',
+    description:
+      'A web app connecting urban stakeholders with an interactive approach using MapBox API, created in a team at Le Wagon bootcamp.',
+  },
+  {
+    name: 'LinkedIn Generator',
+    url: 'https://linkedin-posts-generator.vercel.app/',
+    github: 'https://github.com/PiotrekCodes/nextjs-gpt3',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic eius animi sed. Delectus architecto cupiditate optio ut provident ipsum impedit.',
+  },
+];
+
+export default data;
