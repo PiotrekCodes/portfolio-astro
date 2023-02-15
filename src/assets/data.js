9ad1ce0bc7a52969b73b0a6ee66339addf98536d
Piotr Krawczyk <sztymperjan@gmail.com>
2023-02-15 17:06:36 +0000

    projects section added

--- src/assets/data.js
+++ src/assets/data.js
@@ -5,13 +5,31 @@ const data = [
     github: 'https://github.com/DariBerrie/ouicity',
     description:
       'A web app connecting urban stakeholders with an interactive approach using MapBox API, created in a team at Le Wagon bootcamp.',
+    technologies: ['Ruby on Rails, PostgreSQL'],
   },
   {
     name: 'LinkedIn Generator',
     url: 'https://linkedin-posts-generator.vercel.app/',
     github: 'https://github.com/PiotrekCodes/nextjs-gpt3',
     description:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic eius animi sed. Delectus architecto cupiditate optio ut provident ipsum impedit.',
+      'App built with NextJS, TailwindCSS, Typescript and ChatGPT API. It lets users create a motivational post that they can use on LinkedIn based on their prompt.',
+    technologies: ['NextJS, Typescript, TailwindCSS'],
+  },
+  {
+    name: 'Supanotes',
+    url: 'https://supanotes.netlify.app/',
+    github: 'https://github.com/PiotrekCodes/notesApp-react-supabase',
+    description:
+      'Note taking app built with React and connected to Supabase to store all the notes. User is able to add, edit or delete the notes.',
+    technologies: ['React, Supabase'],
+  },
+  {
+    name: 'Portfolio',
+    url: 'https://www.piotrek.codes',
+    github: 'https://github.com/PiotrekCodes/portfolio-astro',
+    description:
+      'This portfolio website, built with Astro framework for the blazing fast speed it offers. Styled with vanilla CSS.',
+    technologies: ['Astro, CSS'],
   },
 ];
 
